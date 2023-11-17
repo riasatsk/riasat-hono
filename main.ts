4cd2da31481de7b36e11cdede03236fb56ff889b
Riasat <riasatofficial@outlook.com>
2023-11-18 03:33:27 +0530

    modified:   main.ts 	modified:   routes/auth.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 import { Context, Hono } from "https://deno.land/x/hono@v3.10.1/mod.ts";
-import { auth } from "./routes/auth.ts";
-
 const app = new Hono();
-app.use('/auth',auth);
+
+interface User {
+  name: string;
+  password: string;
+}
+
+
+const user: User[] = [];
+
+app.post("/signup", async (c: Context) => {
+  const body = await c.req.json();
+  const name = body.name;
+  const password = body.password;
+  user.push({ name: name, password: password });
+  c.text(name, password);
+});
 
 app.get("/", (c: Context) => c.text("Welcome to Riasat API!"));
 
