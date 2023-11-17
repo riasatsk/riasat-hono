b93e579cdd1de105aa9176d521ca7afc6c6b2507
Riasat <riasatofficial@outlook.com>
2023-11-18 02:34:06 +0530

    new file:   .vscode/settings.json 	deleted:    data.json 	modified:   main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,8 @@
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-
-import data from "./data.json" assert { type: "json" };
+import { Context, Hono } from "https://deno.land/x/hono@v3.10.1/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Welcome to Riasat API!"));
+app.get("/", (c:Context) => c.text("Welcome to Riasat API!"));
 
-app.get("/api/", (c) => c.json(data));
-
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text("No dinosaurs found.");
-  }
-});
 
 Deno.serve(app.fetch);
