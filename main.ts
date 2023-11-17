dc72e8e06c22783a5888e4723881ad500f901a37
Riasat <riasatofficial@outlook.com>
2023-11-18 03:24:13 +0530

    modified:   main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import { Context, Hono } from "https://deno.land/x/hono@v3.10.1/mod.ts";
+import { auth } from "./routes/auth.ts";
 
 const app = new Hono();
 
-app.get("/", (c:Context) => c.text("Welcome to Riasat API!"));
-
+app.get("/", (c: Context) => c.text("Welcome to Riasat API!"));
+app.use(auth);
 
 Deno.serve(app.fetch);
